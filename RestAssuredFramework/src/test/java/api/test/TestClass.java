fd6ae5ccbdb372d10c95ecf64e50817fb54eee22
SHIVKUMAR <SHIVKUMAR@DESKTOP-QNNDG7E>
2023-06-09 05:58:53 +0530

    pom.xml

--- RestAssuredFramework/src/test/java/api/test/TestClass.java
+++ RestAssuredFramework/src/test/java/api/test/TestClass.java
@@ -1,5 +1,17 @@
 package api.test;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class TestClass {
+	
+	//RestAssured.
+	
+	Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+	
+	//System.out.println();
+	
+	// response.getStatusCode();u
+	
 
 }
