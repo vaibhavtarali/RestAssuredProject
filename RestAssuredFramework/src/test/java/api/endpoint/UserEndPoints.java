2b9affff19e732e394a260e2befbbf22960ab6ca
SHIVKUMAR <SHIVKUMAR@DESKTOP-QNNDG7E>
2023-06-09 08:11:42 +0530

    UserEnd point created

--- RestAssuredFramework/src/test/java/api/endpoint/UserEndPoints.java
+++ RestAssuredFramework/src/test/java/api/endpoint/UserEndPoints.java
@@ -8,32 +8,39 @@ import org.seleniumhq.jetty9.server.Authentication.User;
 import io.restassured.response.Response;
 
 public class UserEndPoints {
-	
-	public static Response createUser(User payload)
-	{
-		
-		Response response = given()
-		 .contentType(ContentType.JSON)
-		 .accept(ContentType.JSON)
-		.when()
-		   .post(Routes.post_url);
-		
+
+	public static Response createUser(User payload) {
+
+		Response response = given().contentType(ContentType.JSON).accept(ContentType.JSON).when().post(Routes.post_url);
+
 		return response;
-		
+
 	}
-	
-	public static Response readUser(String userName)
-	{
-		
-		Response response = given()
-				.pathParam("username", userName)
-		 .contentType(ContentType.JSON)
-		 .accept(ContentType.JSON)
-		.when()
-		   .post(Routes.get_url);
-		
+
+	public static Response readUser(String userName) {
+
+		Response response = given().pathParam("username", userName).contentType(ContentType.JSON)
+				.accept(ContentType.JSON).when().get(Routes.get_url);
+
 		return response;
-		
+
+	}
+
+	public static Response updateUser(String userName, User payload) {
+
+		Response response = given().contentType(ContentType.JSON).accept(ContentType.JSON)
+				.pathParam("userName", userName).body(payload).when().put(Routes.update_url);
+
+		return response;
+
+	}
+
+	public static Response deleteUser(String userName) {
+
+		Response response = given().pathParam("username", userName).when().delete(Routes.delete_url);
+
+		return response;
+
 	}
 
 }
