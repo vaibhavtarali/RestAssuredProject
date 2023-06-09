3127d37651ca8d6724906e230b6f57053990ce4b
SHIVKUMAR <SHIVKUMAR@DESKTOP-QNNDG7E>
2023-06-09 14:52:12 +0530

    create class

--- RestAssuredFramework/src/test/java/api/endpoint/UserEndPoints.java
+++ RestAssuredFramework/src/test/java/api/endpoint/UserEndPoints.java
@@ -3,15 +3,19 @@ package api.endpoint;
 import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 
-import org.seleniumhq.jetty9.server.Authentication.User;
-
+import api.payload.User;
 import io.restassured.response.Response;
 
 public class UserEndPoints {
 
 	public static Response createUser(User payload) {
 
-		Response response = given().contentType(ContentType.JSON).accept(ContentType.JSON).when().post(Routes.post_url);
+		Response response = given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.body(payload)
+			.when()
+			     .post(Routes.post_url);
 
 		return response;
 
@@ -19,8 +23,12 @@ public class UserEndPoints {
 
 	public static Response readUser(String userName) {
 
-		Response response = given().pathParam("username", userName).contentType(ContentType.JSON)
-				.accept(ContentType.JSON).when().get(Routes.get_url);
+		Response response = given()
+				.pathParam("username", userName)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+			.when()
+				.get(Routes.get_url);
 
 		return response;
 
@@ -28,8 +36,13 @@ public class UserEndPoints {
 
 	public static Response updateUser(String userName, User payload) {
 
-		Response response = given().contentType(ContentType.JSON).accept(ContentType.JSON)
-				.pathParam("userName", userName).body(payload).when().put(Routes.update_url);
+		Response response = given()
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+				.pathParam("userName", userName)
+				.body(payload)
+			.when()
+				.put(Routes.update_url);
 
 		return response;
 
@@ -37,7 +50,10 @@ public class UserEndPoints {
 
 	public static Response deleteUser(String userName) {
 
-		Response response = given().pathParam("username", userName).when().delete(Routes.delete_url);
+		Response response = given()
+				.pathParam("username", userName)
+			.when()
+				.delete(Routes.delete_url);
 
 		return response;
 
--- RestAssuredFramework/src/test/java/api/payload/payloadclass.java
+++
@@ -1,5 +0,0 @@
-package api.payload;
-
-public class payloadclass {
-
-}
