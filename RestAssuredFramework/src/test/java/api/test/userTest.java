379ccf57e4bb6916297860ba50e54a72a4746bfa
SHIVKUMAR <SHIVKUMAR@DESKTOP-QNNDG7E>
2023-06-13 09:44:52 +0530

    create user test

---
+++ RestAssuredFramework/src/test/java/api/test/userTest.java
@@ -0,0 +1,27 @@
+package api.test;
+
+import org.testng.annotations.BeforeClass;
+
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class userTest {
+	
+	//RestAssured.
+	
+	//Response response = RestAssured.get("https://reqres.in/api/users?page=2");
+	
+	//Faker faker;
+	//System.out.println();
+	
+	// response.getStatusCode();
+	@BeforeClass
+	public  void setUpData() {
+		
+		
+	}
+	
+	
+
+}
